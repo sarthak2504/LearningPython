c6d20646ce2d53afb4db9f962f0fa11e67fdee5a
sarthak2504 <sarthak.bhingarde2504@gmail.com>
2020-12-05 01:19:03 +0530

    Pandas_1 created to learn pandas

---
+++ Pandas_1.py
@@ -0,0 +1,24 @@
+import pandas as pd
+
+# Reading of different kinds of files using Pandas
+
+df = pd.read_csv('pokemon_data.csv')
+
+df_xlsx = pd.read_excel('pokemon_data.xlsx')
+
+df_tab = pd.read_csv('pokemon_data.txt', delimiter='\t')
+
+#print(df.head(5))
+
+#print(df[['Name','Type 1','#']])
+
+#print(df.iloc[1])
+
+#print(df.iloc[0,1])
+
+# TO ITERATE THROUGH THE DATA
+
+#for index,rows in df.iterrows():
+#    print(index,rows['Name'])
+
+print(df.loc[df['Type 1'] == "Water"][df['Type 2'] == "Flying"])
